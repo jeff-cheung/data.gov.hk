3a9465894070b275bcc419e9cc2ad060e9e8be70
Jeff Cheung <jeff.cwcheung@gmail.com>
2019-07-14 16:32:50 +0800

    new file:   python/draw_networkx_graph.py modified:   python/load_gtfs_to_avro.py

--- python/draw_networkx_graph.py
+++ python/draw_networkx_graph.py
@@ -1,17 +1,10 @@
-import avro.schema
+import networkx as nx
+import pandas as pd
 from pathlib import Path
 from datetime import date, datetime
-import pandas as pd
 from shapely.geometry import Point
-from psycopg2.extensions import adapt, register_adapter, AsIs
-
-from avro.datafile import DataFileReader, DataFileWriter
-from avro.io import DatumReader, DatumWriter
-
-def adapt_point(point: Point):
-    x = adapt(point.x).getquoted().decode()
-    y = adapt(point.y).getquoted().decode()
-    return AsIs("'(%s, %s)'" % (x, y))
+import matplotlib.pyplot as plt
+import re
 
 def retrieve_data_last_upd_date(filename='DATA_LAST_UPDATED_DATE.csv'):
     data_folder = Path('gtfs_data')
@@ -22,18 +15,21 @@ def retrieve_data_last_upd_date(filename='DATA_LAST_UPDATED_DATE.csv'):
         last_upd_dt = f.readline()
         #print(last_upd_dt)
         return datetime.strptime(last_upd_dt, "%Y-%m-%d").date()
-        
+
 def read_gtfs_data(data_type):
     data_file = data_type + '.txt'
     data_folder = Path('gtfs_data')
     path = data_folder / data_file
-    raw_data = pd.read_csv(path.resolve()).fillna('')
+    raw_data = pd.read_csv(path.resolve())#.fillna('')
     last_upd_dt = retrieve_data_last_upd_date()
-    raw_data['as_of_date'] = last_upd_dt.toordinal() - date(1970, 1, 1).toordinal()
+    raw_data['as_of_date'] = last_upd_dt
 
     return refine_data(raw_data, data_type)
 
 def refine_data(raw_data, data_type):
+    if data_type == 'routes':
+        raw_data.set_index('route_id')
+
     if data_type == 'stops':
         geom = [Point(xy) for xy in zip(raw_data.stop_lon, raw_data.stop_lat)]
         raw_data['stop_lat_lon'] = geom
@@ -79,59 +75,56 @@ def refine_data(raw_data, data_type):
 
     return raw_data
 
-def write_data_to_avro(raw_data, data_type):
+def generate_edge(df):
+    for index in range(len(df)-1, 0, -1):
+        yield(df[index-1], df[index])
+
+def generate_stops(route_no):
+    routes_data = read_gtfs_data('routes')
+    trips_data = read_gtfs_data('trips')
+    stops_data = read_gtfs_data('stops')
+
+    selected_route_id = routes_data[routes_data.route_short_name == route_no].route_id.values[0]
+    selected_agency_id = routes_data[routes_data.route_short_name == route_no].agency_id.values[0]
+
+    selected_trip_id = trips_data[trips_data.route_id == selected_route_id].trip_id.values[0]
+
+    stop_times_data = read_gtfs_data('stop_times')
+    stop_times_data = stop_times_data[stop_times_data.trip_id == selected_trip_id]
+
+    pd_merge = pd.merge(stop_times_data, stops_data, on='stop_id')
+
+    stop_name_dict = pd_merge[['stop_id', 'stop_name']].set_index('stop_id').to_dict()['stop_name']
+
+    dict = {}
+    re_exp = re.compile('\[(?P<agency>...*)\] (?P<stop_name>...*)')
+    for id, name in iter(stop_name_dict.items()):
+        #dict[id] = name.split('|')
+        for n in name.split('|'):
+            if (id, re_exp.match(n).groupdict()['agency']) in dict.keys():
+                dict[(id, re_exp.match(n).groupdict()['agency'])] = dict[(id, re_exp.match(n).groupdict()['agency'])] + \
+                    ' / ' + re_exp.match(n).groupdict()['stop_name']
+            else:
+                dict[(id, re_exp.match(n).groupdict()['agency'])] = re_exp.match(n).groupdict()['stop_name']
     
-    data_folder = Path('avro')
-    avro_file = data_type + '.avro'
-    avro_file_path = data_folder / avro_file
-    avsc_file = data_type + '.avsc'
-    avsc_file_path = data_folder / avsc_file
-
-    schema = avro.schema.Parse(open(avsc_file_path.resolve(), "rb").read())
-
-    writer = DataFileWriter(open(avro_file_path.resolve(), "wb"), DatumWriter(), schema)
-
-    for _ , record in raw_data.iterrows():
-        dict = record.to_dict()
-
-        if data_type == "stops":
-            dict['stop_lat_lon'] = {'stop_lon': dict['stop_lat_lon'].x, 'stop_lat': dict['stop_lat_lon'].y}
-        '''
-        if data_type == "stop_times":
-            #del dict['arrival_time']
-            #del dict['departure_time']
-            del dict['stop_id']
-            del dict['stop_sequence']
-            del dict['pickup_type']
-            del dict['drop_off_type']
-            del dict['timepoint']
-        '''
-        writer.append(dict)
+    for k in stop_name_dict.keys():
+        stop_name_dict[k] = dict[(k, selected_agency_id)]
     
-    writer.close()
-    '''
-    reader = DataFileReader(open(path.resolve(), "rb"), DatumReader())
-    for agency in reader:
-        print(agency)
+    return stop_name_dict
 
-    reader.close()
-    '''
 
 if __name__ == '__main__':
-    register_adapter(Point, adapt_point)
 
-    #agency_data = read_gtfs_data('agency')
-    #write_data_to_avro(agency_data, 'agency')
+    stop_name_dict = generate_stops('962X')
 
-    #stops_data = read_gtfs_data('stops')
-    #write_data_to_avro(stops_data, 'stops')
+    print(list(stop_name_dict))
 
-    #routes_data = read_gtfs_data('routes')
-    #write_data_to_avro(routes_data, 'routes')
+    G = nx.DiGraph()
+    G.add_edges_from(generate_edge(list(stop_name_dict)))
 
-    #stop_times_data = read_gtfs_data('stop_times')
-    #write_data_to_avro(stop_times_data, 'stop_times')
+    print(G.nodes.data())
 
-    trips_data = read_gtfs_data('trips')
-    write_data_to_avro(trips_data, 'trips')
-
+    nx.relabel_nodes(G, stop_name_dict, copy=False)
+    nx.draw(G, with_labels=True, font_size=6)
+    plt.show()
+    
